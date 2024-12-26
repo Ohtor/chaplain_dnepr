3ba1625461a7288d547063c0361f530b99389d65
Ohtor <aonaxionai@gmail.com>
2024-12-26 06:48:36 +0200

    fix footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,30 +1,30 @@
 <div class="footer">
     <div class="column1">
-        <a href="#" class="footerlink">Контакти<br></a>
+        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
         <a href="{{ route('mainarticle.about_us.index') }}" class="footerlink">Про нас<br></a>
         <a href="#" class="footerlink">Статут<br></a>
-        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
+        <a href="#" class="footerlink">Контакти<br></a>
         <a href="#" class="footerlink">Популярні питання<br></a>
     </div>
     <div class="column2">
-        <a href="#" class="footerlink">Контакти<br></a>
+        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
         <a href="{{ route('mainarticle.about_us.index') }}" class="footerlink">Про нас<br></a>
         <a href="#" class="footerlink">Статут<br></a>
-        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
+        <a href="#" class="footerlink">Контакти<br></a>
         <a href="#" class="footerlink">Популярні питання<br></a>
     </div>
     <div class="column3">
-        <a href="#" class="footerlink">Контакти<br></a>
+        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
         <a href="{{ route('mainarticle.about_us.index') }}" class="footerlink">Про нас<br></a>
         <a href="#" class="footerlink">Статут<br></a>
-        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
+        <a href="#" class="footerlink">Контакти<br></a>
         <a href="#" class="footerlink">Популярні питання<br></a>
     </div>
     <div class="column4">
-        <a href="#" class="footerlink">Контакти<br></a>
+        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
         <a href="{{ route('mainarticle.about_us.index') }}" class="footerlink">Про нас<br></a>
         <a href="#" class="footerlink">Статут<br></a>
-        <a href="{{ route('mainarticle.interaction_with_authorities.index') }}" class="footerlink">Взаємодія з владою<br></a>
+        <a href="#" class="footerlink">Контакти<br></a>
         <a href="#" class="footerlink">Популярні питання<br></a>
     </div>
 </div>
